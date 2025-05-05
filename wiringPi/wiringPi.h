512700c41c48ca767f3a12832e1ed4595e1f1eec
Connor Gibson <connor@nextdoortech.net>
2025-05-04 19:51:54 -0700

    Add `delayNanoseconds()`, rework existing delay functions
     - added internal inline helper function `delayHelper()` as wrapper for `clock_nanosleep()`
     - added internal inline helper function `delayHelperHard()` as wrapper for hard loop sleep
     - deprecated `delayMicrosecondsHard()` in favor of `delayHelperHard()` functionality

--- wiringPi/wiringPi.h
+++ wiringPi/wiringPi.h
@@ -320,8 +320,9 @@ extern int piHiPri (const int pri) ;
 
 // Extras from arduino land
 
-extern void         delay             (unsigned int ms) ;
-extern void         delayMicroseconds (unsigned int us) ;
+extern void         delay             (unsigned int howLong_ms); // Wait for some number of milliseconds
+extern void         delayMicroseconds (unsigned int howLong_us); // Wait for some number of microseconds
+extern void         delayNanoseconds  (unsigned int howLong_ns); // Wait for some number of nanoseconds
 extern unsigned int millis            (void) ;
 extern unsigned int micros            (void) ;
 
